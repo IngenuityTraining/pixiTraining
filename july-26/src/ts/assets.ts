4a038f4e547de685d57fac9d9f40663c33d3864e
ankush-badyal-23 <86702078+ankush-badyal-23@users.noreply.github.com>
2021-07-26 11:46:10 +0530

    images and preloader

---
+++ july-26/src/ts/assets.ts
@@ -0,0 +1,25 @@
+export default {
+  baseUrl: './assets/img',
+  images: [
+    {
+      key: 'goku_blue',
+      url: 'gokublue.jpg',
+    },
+    {
+      key: 'goku_ui',
+      url: 'ultraInstinct.jpg',
+    },
+    {
+      key: 'vegita_blue',
+      url: 'vegitaBlue.jpg',
+    },
+    {
+      key: 'vegita_super_blue',
+      url: 'vegitaSuperBlue.jpg',
+    },
+    {
+      key: 'car',
+      url: 'car1.png',
+    },
+  ],
+};
